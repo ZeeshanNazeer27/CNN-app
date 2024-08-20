fdfacc2809bc18d18a364b645e1b95ef9ce1938e
ZeeshanNazeer27 <115093169+ZeeshanNazeer27@users.noreply.github.com>
2024-08-20 06:15:03 +0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -2,28 +2,33 @@ import streamlit as st
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import load_model
-import cv2
 import numpy as np
 from PIL import Image
 
-model = load_model('E:\Internship\CNN\cnn.h5')
+# Load the model
+model = load_model('E:/Internship/CNN/cnn.h5')
 
 st.title("Cheetah or Hyena Classifier")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
+    # Open and display the uploaded image
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
     
+    # Resize the image to 256x256 using Pillow
+    image = image.resize((256, 256))
+    
+    # Convert the image to a numpy array and normalize it
     image = np.array(image)
-    image = cv2.resize(image, (256, 256))
-    image = image / 255.0 
+    image = image / 255.0
+    
+    # Reshape the image for the model
     image = image.reshape(1, 256, 256, 3)
 
+    # Predict using the model
     prediction = model.predict(image)
     if prediction < 0.5:
         st.write("Prediction: Cheetah")
     else:
         st.write("Prediction: Hyena")
-
-
