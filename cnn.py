f1aa48a5001de924f8f7739b434d57f57e7c9b5e
ZeeshanNazeer27 <115093169+ZeeshanNazeer27@users.noreply.github.com>
2024-08-20 06:22:46 +0500

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 import os
 
-# Load model
+
 model_path = 'cnn.h5'
 if os.path.exists(model_path):
     model = load_model(model_path)
@@ -16,21 +16,17 @@ st.title("Cheetah or Hyena Classifier")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Open and display the uploaded image
+    
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image', use_column_width=True)
     
-    # Resize the image to 256x256 using Pillow
+   
     image = image.resize((256, 256))
-    
-    # Convert the image to a numpy array and normalize it
     image = np.array(image)
     image = image / 255.0
     
-    # Reshape the image for the model
     image = image.reshape(1, 256, 256, 3)
 
-    # Predict using the model
     prediction = model.predict(image)
     if prediction < 0.5:
         st.write("Prediction: Cheetah")
